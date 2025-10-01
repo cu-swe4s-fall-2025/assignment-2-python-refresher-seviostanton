e9d06adbb09d3a588b7abc3e67884933e8c10bff
Sevio Stanton <Sevio.Stanton@colorado.edu>
2025-09-30 23:41:39 -0600

    update test_print

--- test_print_fires.sh
+++ test_print_fires.sh
@@ -22,22 +22,21 @@ EXPECTED_RAW="Values from column 7 for Italy: [1744, 1603, 1571, 1398]"
 EXPECTED_MEAN="mean of values from column 7 for Italy: 1579.0"
 EXPECTED_MEDIAN="median of values from column 7 for Italy: 1587.0"
 EXPECTED_STD="std of values from column 7 for Italy: 142.15718999286202"
-EXPECTED_FILE_NOT_FOUND="Error: File 'does_not_exist.csv' not found."
-
+EXPECTED_CONVERT_ERR="ValueError: Could not convert data in row to int: Italy"
 # ---- tests ----
-run raw_data python3 $SCRIPT --file_name $DATA --country "$COUNTRY" --country_column $COUNTRY_COLUMN --fires_column $FIRES_COLUMN
+run raw_data python3 $SCRIPT --file_name $DATA --country $COUNTRY --country_column $COUNTRY_COLUMN --fires_column $FIRES_COLUMN
 assert_exit_code 0
 assert_in_stdout "$EXPECTED_RAW"
 
-run mean_data python3 $SCRIPT --file_name $DATA --country "$COUNTRY" --country_column $COUNTRY_COLUMN --fires_column $FIRES_COLUMN --operation "mean"
+run mean_data python3 $SCRIPT --file_name $DATA --country $COUNTRY --country_column $COUNTRY_COLUMN --fires_column $FIRES_COLUMN --operation "mean"
 assert_exit_code 0
 assert_in_stdout "$EXPECTED_MEAN"
 
-run median_data python3 $SCRIPT --file_name $DATA --country "$COUNTRY" --country_column $COUNTRY_COLUMN --fires_column $FIRES_COLUMN --operation "median"
+run median_data python3 $SCRIPT --file_name $DATA --country $COUNTRY --country_column $COUNTRY_COLUMN --fires_column $FIRES_COLUMN --operation "median"
 assert_exit_code 0
 assert_in_stdout "$EXPECTED_MEDIAN"
 
-run std_data python3 $SCRIPT --file_name $DATA --country "$COUNTRY" --country_column $COUNTRY_COLUMN --fires_column $FIRES_COLUMN --operation "std"
+run std_data python3 $SCRIPT --file_name $DATA --country $COUNTRY --country_column $COUNTRY_COLUMN --fires_column $FIRES_COLUMN --operation "std"
 assert_exit_code 0
 assert_in_stdout "$EXPECTED_STD"
 
@@ -45,6 +44,3 @@ run no_country python3 $SCRIPT --file_name $DATA --country "Narnia" --country_co
 assert_exit_code 0
 assert_in_stdout "Values from column $FIRES_COLUMN for Narnia: []"
 
-run missing_file python3 "$SCRIPT" --file_name does_not_exist.csv --country "$COUNTRY" --country_column $COUNTRY_COLUMN --fires_column $FIRES_COLUMN
-assert_exit_code 1
-assert_in_stderr "$EXPECTED_FILE_NOT_FOUND"
